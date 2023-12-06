10630deb83959b7fa303deef6512bf4238cc454b
AbdelazizSleem01 <134790992+AbdelazizSleem01@users.noreply.github.com>
2023-12-07 01:49:33 +0200

    changes

--- clock.js
+++ clock.js
@@ -1,27 +1,22 @@
 // clock
+function show() {
+    var d = new Date();
 
-function show () {
-    var d = new Date ();
-
-    var hours = d.getHours() ;
-    var minut = d.getMinutes() ;
-    var Seconds = d.getSeconds() ;
+    var hours = d.getHours();
+    var minut = d.getMinutes();
+    var Seconds = d.getSeconds();
     var ampm = hours >= 12 ? 'pm' : 'am';
 
 
-minut = (minut < 10) ? "0" + minut : minut;
-Seconds = (Seconds < 10) ? "0" + Seconds : Seconds;
-hours = (hours > 12) ?  hours - "12"  : hours;
+    minut = (minut < 10) ? "0" + minut : minut;
+    Seconds = (Seconds < 10) ? "0" + Seconds : Seconds;
+    hours = (hours > 12) ? hours - "12" : hours;
 
 
-var time = `${hours} : ${minut} : ${Seconds} ${ampm}`
+    var time = `${hours} : ${minut} : ${Seconds} ${ampm}`
 
-setTimeout(show , 1000)
+    setTimeout(show, 1000)
 
-document.getElementById("clock").innerHTML = time ;
+    document.getElementById("clock").innerHTML = time;
 }
-show ()
-
-
- 
- 
+show()
